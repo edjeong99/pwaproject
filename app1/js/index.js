f5aabc4986ef42cf7cd0e51d3f6695967f873100
Edward J <edjeong99@gmail.com>
2020-01-13 20:39:07 -0600

    create element for input, apply css

--- app1/js/index.js
+++ app1/js/index.js
@@ -1,7 +1,23 @@
 //alert('JS working');
 
+var inputArr = [];
+
+function InputItem(inputStr) {
+  this.inputStr = inputStr;
+  this.date = new Date();
+}
+
 function addInput() {
-  document.getElementById('divResult').innerHTML = document.getElementById(
-    'inputtest'
-  ).value;
+  //     document.getElementById('divResult').innerHTML = document.getElementById(
+  //     'inputtest'
+  //   ).value;
+
+  var inputStr = document.getElementById('inputtest').value;
+  inputArr.push(new InputItem(inputStr));
+  //alert(inputArr[inputArr.length - 1].inputStr);
+
+  var node = document.createElement('span');
+  node.innerHTML = inputStr;
+  node.setAttribute('class', 'inputStrDisplay');
+  document.getElementById('divResult').appendChild(node);
 }
