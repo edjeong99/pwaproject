87065d48c04d74eee31e4958a8c69f0368ba1d70
Edward J <edjeong99@gmail.com>
2020-01-15 20:48:37 -0600

    display inputs using objArray in  localStorage

--- app1/js/index.js
+++ app1/js/index.js
@@ -1,5 +1,3 @@
-//alert('JS working');
-
 var inputArr = [];
 
 function InputItem(inputStr) {
@@ -8,24 +6,32 @@ function InputItem(inputStr) {
 }
 
 function addInput() {
-  //     document.getElementById('divResult').innerHTML = document.getElementById(
-  //     'inputtest'
-  //   ).value;
-
   var inputStr = document.getElementById('inputtest').value;
-
-  spanMaker(inputStr);
 
   inputArr.push(new InputItem(inputStr));
 
   // save the inputArr in local storage
   // convert array into string for local storage
   localStorage.setItem('inputStr', JSON.stringify(inputArr));
+
+  displayInput2();
 }
 
 const spanMaker = inputStr => {
   const span = document.createElement('span');
   span.textContent = inputStr;
   span.setAttribute('class', 'inputStrDisplay');
+  return span;
+};
+
+const displayInput = inputStr => {
+  var span = spanMaker(inputStr);
   document.getElementById('divResult').appendChild(span);
 };
+
+function displayInput2() {
+  document.getElementById('divResult').innerHTML = '';
+  for (let i = 0; i < inputArr.length; i++) {
+    displayInput(inputArr[i].inputStr);
+  }
+}
